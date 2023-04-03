5bfed077b4407b670e3261071cf35ebf6efc4568
geekynjoki <gathuranjoki18@gmail.com>
2023-04-03 11:43:46 +0300

    tours

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
+import app from "./app";
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<app/>)
